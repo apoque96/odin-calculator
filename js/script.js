1aeab4d7c666f2f2b5a9043fbe6490368497674e
apoque96 <dereckcabrerang@gmail.com>
2024-01-24 20:44:53 -0600

    Operations working

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-let firstNumber = 0;
-let secondNumber = 0;
+let result = "";
+let currentOperator = 0;
 
 const DISPLAY = document.querySelector("#display");
 
@@ -7,6 +7,13 @@ const numbers = document.querySelectorAll(".numbers");
 for(let i = 0; i < numbers.length; i++){
     numbers[i].addEventListener('click', (e) => updateDisplay(e.target.textContent));
 }
+const operators = document.querySelectorAll(".operators");
+for(let i = 0; i < operators.length; i++){
+    operators[i].addEventListener('click', (e) => operatorClick(e.target.textContent));
+}
+
+document.querySelector("#equals").addEventListener('click', 
+    () => operate(result, DISPLAY.textContent, currentOperator));
 
 const OPERATOR ={
     none: 0,
@@ -14,6 +21,13 @@ const OPERATOR ={
     subtract: 2,
     multiply: 3,
     divide: 4
+}
+
+function reset(){
+    result = "";
+    secondNumber = "";
+    currentOperator = 0;
+    updateDisplay("");    
 }
 
 function add(a, b){
@@ -29,26 +43,56 @@ function multiply(a, b){
 }
 
 function divide(a, b){
-    b == 0 ? "You stoopid" : a / b;
+    return b == 0 ? "You stoopid" : a / b;
 }
 
 function operate(numberA, numberB, operator){
+    numberA = Number(numberA);
+    numberB = Number(numberB);
     switch(operator){
         case OPERATOR.add:
-            add(numberA, numberB)
+            result = add(numberA, numberB)
             break;
         case OPERATOR.subtract:
-            subtract(numberA, numberB)
+            result = subtract(numberA, numberB)
             break;
         case OPERATOR.multiply:
-            multiply(numberA, numberB)
+            result = multiply(numberA, numberB)
             break;
         case OPERATOR.divide:
-            divide(numberA, numberB)
+            result = divide(numberA, numberB)
             break;
     }
+
+    resetDisplay();
+    updateDisplay(result);
+}
+
+function operatorClick(operator){
+    if(DISPLAY.textContent.length === 0) return;
+    switch(operator){
+        case '+':
+            currentOperator = OPERATOR.add;
+            break;
+        case '-':
+            currentOperator = OPERATOR.subtract;
+            break;
+        case 'x':
+            currentOperator = OPERATOR.multiply;
+            break;
+        case '/':
+            currentOperator = OPERATOR.divide;
+            break;
+    }
+    if(result.length === 0)
+        result = DISPLAY.textContent;
+    resetDisplay();
+}
+
+function resetDisplay(){
+    DISPLAY.textContent = '';
 }
 
 function updateDisplay(val){
-    DISPLAY.textContent += val;
+    if(val != '0')DISPLAY.textContent += val;
 }
